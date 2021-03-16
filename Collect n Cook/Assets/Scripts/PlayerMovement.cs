db9549d0282d5dacb699ff1bb67cb0823b9a2fcd
Noel <1015895@student.sae.edu.au>
2021-03-16 19:08:11 +1100

    Auto stash before merge of "main" and "origin/main"
    Third Person Camera, New Map

--- Collect n Cook/Assets/Scripts/PlayerMovement.cs
+++ Collect n Cook/Assets/Scripts/PlayerMovement.cs
@@ -13,21 +13,33 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 velocity;
 
-    public Animator animator;
+    [SerializeField]
+    Animator animator;
 
     float walkingV;
-    
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
+
+    [SerializeField]
+    Transform groundCheck;
+
+    [SerializeField]
+    float groundDistance = 0.4f;
+
+    [SerializeField]
+    LayerMask groundMask;
     //public float waterDistance = 0.4f;
     //public LayerMask waterMask;
     bool isGrounded;
-    public bool isWatered;
+    //public bool isWatered;
 
-    public float downTime, upTime, pressTime = 0;
-    public float countDown = 5.0f;
-    
+    float downTime, pressTime = 0;
+    float countDown = 5.0f;
+
+    [SerializeField]
+    float turnSmoothTime = 0.1f;
+
+    float turnSmoothVelocity;
+    [SerializeField]
+    Transform cam;
 
     void Start()
     {
@@ -50,45 +62,50 @@ public class PlayerMovement : MonoBehaviour
             Debug.Log("In water");
         }
      */
-
-
         float x = Input.GetAxis("Horizontal");
-       
         walkingV = Input.GetAxis("Vertical");
   
         animator.SetFloat("Walking", Mathf.Abs(walkingV));
 
-        Vector3 move = transform.right * x + transform.forward * walkingV;
-        controller.Move(move * speed * Time.deltaTime);
+        //Vector3 move = (transform.right * x + transform.forward * walkingV).normalized;
 
+        Vector3 move = new Vector3(x, 0f, walkingV).normalized;
+        
+        //Player rotation left and right
+        if (move.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        //Animation if statement for running
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-
             downTime = Time.time;
             pressTime = downTime + countDown;
             
             animator.SetFloat("Running", 1f);
             speed += speedBoost;
-
         }
 
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-
             animator.SetFloat("Running", 0f);
 
             if (Time.time >= pressTime)
             {
-
                 //SoundManager.PlaySFX("HBreathing");
             }
 
             //SoundManager.PlaySFX("HBreathing");
-
-            speed -= speedBoost;
-           
+            speed -= speedBoost;        
         }
 
+        //Animation if statement for jumping
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             //SoundManager.PlaySFX("JumpLanding");
