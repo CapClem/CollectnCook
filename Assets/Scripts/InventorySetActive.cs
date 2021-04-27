a5435ce5678555dcf09cfcdd005f148f8391613f
Jacob <1014906@student.sae.edu.au>
2021-04-27 15:59:15 +1000

    Cooking UI Activation
    etc

--- Assets/Scripts/InventorySetActive.cs
+++ Assets/Scripts/InventorySetActive.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class InventorySetActive : MonoBehaviour
 {
     public GameObject setThisActive;
+    public PotInteraction cookingModeScript;
 
     void Start()
     {
@@ -16,13 +17,14 @@ public class InventorySetActive : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    { 
-        if (Input.GetKeyDown(KeyCode.I))
+    {
+        if (Input.GetKeyDown(KeyCode.I) && cookingModeScript.cookingMode == false)
         {
+            //check if you are in cooking mode and if cooking mode = true, return
             setThisActive.SetActive(true);
         }
 
-        else if (Input.GetKeyDown(KeyCode.O))
+        else if (Input.GetKeyDown(KeyCode.O)) 
         {
             setThisActive.SetActive(false);
         }
--- Assets/Scripts/PotInteraction.cs
+++ Assets/Scripts/PotInteraction.cs
@@ -6,14 +6,42 @@ public class PotInteraction : MonoBehaviour
 {
     public GameObject UIObject;
     public GameObject UIbackground;
+    public GameObject MapCamera;
+    public GameObject CookingCamera;
+    public GameObject CookBookPanel;
+    public GameObject InventoryPanel;
+    public bool active;
+    public GameObject Player;
+    public GameObject Inventory;
+    public bool cookingMode;
 
     void Start()
     {
 
         UIObject.SetActive(false);
         UIbackground.SetActive(false);
+        CookingCamera.SetActive(false);
+        CookBookPanel.SetActive(false);
+        InventoryPanel.SetActive(false);
     }
+    void Update()
+    {
+        if(active && (Input.GetKeyDown(KeyCode.C)))
+        {
+            MapCamera.SetActive(false);
+            CookingCamera.SetActive(true);
+            CookBookPanel.SetActive(true);
+            InventoryPanel.SetActive(true);
+            Debug.Log("cooking activated");
+            UIObject.SetActive(false);
+            UIbackground.SetActive(false);
+            Player.SetActive(false);
+            Debug.Log("it's all inactive, cook for your life");
+            Inventory.SetActive(false);
+            cookingMode = true; //make sure when cooking is closed this is set to false
 
+        }
+    }
 
     void OnTriggerEnter(Collider player)
     {
@@ -21,7 +49,9 @@ public class PotInteraction : MonoBehaviour
         {
             UIObject.SetActive(true);
             UIbackground.SetActive(true);
+            active = true;
         }
+      
 
     }
     void OnTriggerExit(Collider player)
@@ -30,7 +60,7 @@ public class PotInteraction : MonoBehaviour
         {
             UIObject.SetActive(false);
             UIbackground.SetActive(false);
+            active = false;
         }
     }
-
 }
